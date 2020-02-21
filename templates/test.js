95b03797e8d6fe6659e0fda127dca81f039aac9a
ClemHub <clemenceblachon@gmail.com>
2020-02-21 17:15:52 +0100

    Django test

--- templates/test.js
+++
@@ -1,8 +0,0 @@
-function form_submission() {
-    var dama = document.querySelectorAll('input[type=radio]:checked');
-	console.log(dama);
-	var sequences = document.getElementById('sequences').value; 
-	alert(sequences)
-	var pfam_domains = document.getElementById('pfam_domains').value; 
-	alert(pfam_domains)
-}

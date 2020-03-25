e9596dcbed8e0976e7f02bef7fd29ae1929a10fd
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-03-25 10:28:19 +0100

    modifications

--- MyCLADE/submit.php
+++ MyCLADE/submit.php
@@ -7,7 +7,28 @@
 		<p class = 'text'>
 		<?php 
 
-		/*if($_GET['form']=='large'){
+		function generateRandomString($length = 10) {
+			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			$charactersLength = strlen($characters);
+			$randomString = '';
+			for ($i = 0; $i < $length; $i++) {
+				$randomString .= $characters[rand(0, $charactersLength - 1)];}
+			return $randomString;};
+
+		function submit($jobid, $email){
+			//ARGS is the list of arguments you have extracted from your form. Only this is escaped because it is the only things given by the user.
+			$command="qsub -w $approot/jobs -N $jobid $approot/run.sh ".escapeshellarg(ARGS); 
+			//Sublit your job
+			shell_exec("$command");
+			$link="$appurl/status.php?jobid=$jobid"; #status.php is a page that show he status of your job
+			$msg="<strong>Your job has been correctly submitted</strong><br><br>";
+			$msg= $msg . "You can follow job progress as well as downloading the results going to <a target=_blank href=$link> $link </a><br>";
+			$msg= $msg . "<br>Your data will be removed one month after the end of the job.<br>";
+			$msg= $msg . "The job will be stopped if longer than 48 hours.<br>";
+			$msg= $msg . "If you need some help, contact the web developer ($webdevel).<br>";
+			return $msg;};
+
+		if($_GET['form']=='large'){
 				$sequences = $_POST['sequences'];
 				file_put_contents('./fasta_file/fasta_tmp.fa', $sequences);
 
@@ -15,13 +36,8 @@
 				$dama = $_SESSION['dama'];
 				if($dama){
 					$DAMA_evalue = $_SESSION['DAMA-evalue'];}
-				$e_value = $_SESSION['evalue'];
+				$e_value = $_SESSION['evalue'];}
 
-				MetaCLADE program
-				if($dama){./metaclade2/metaclade2 -i ./fasta_file/fasta_temp.fa -N results -d $pfam -W ../ -j 2}
-				else{./metaclade2/metaclade2 -i ./fasta_file/fasta_temp.fa -a -N results -d $pfam -W ../ -j 2}
-
-		}
 		else if($_GET['small']=='large'){
 				$sequences = $_POST['sequences'];
 				file_put_contents('./fasta_file/fasta_tmp.fa', $sequences);
@@ -29,29 +45,23 @@
 				$dama = $_SESSION['dama'];
 				if($dama){
 					$DAMA_evalue = $_SESSION['DAMA-evalue'];}
-				$e_value = $_SESSION['evalue'];
-				
-				MetaCLADE program
-				if($dama){./metaclade2/metaclade2 -i ./fasta_file/fasta_temp.fa -N results -d $pfam -W ../ -j 2}
-				else{./metaclade2/metaclade2 -i ./fasta_file/fasta_temp.fa -a -N results -d $pfam -W ../ -j 2}
-
-		} */
+				$e_value = $_SESSION['evalue'];}
 
 		$jobid = generateRandomString();
 
 		$msg=submit($jobid, $email);
 
-		$email = $_POST['email']; //email is a field with the email of the user. If you don't have, just remove this piece of code
+		$email = $_POST['email'];
 		if($email){                                                                                                                                                                        
-		$mail_header= "Subject: $appname queued ( $jobid )\n";
-		$mail_header= $mail_header . "Content-Type: text/html\n";
-		$mail_header= $mail_header . "MIME-Version:\n";
-		$mail= $mail_header . "\n".$msg;
-		sendMail($mail, "no-reply@lcqb.upmc.fr", $email);
-		};
+			$mail_header= "Subject: $appname queued ( $jobid )\n";
+			$mail_header= $mail_header . "Content-Type: text/html\n";
+			$mail_header= $mail_header . "MIME-Version:\n";
+			$mail= $mail_header . "\n".$msg;
+			sendMail($mail, "no-reply@lcqb.upmc.fr", $email);};
 
 		header("location: $hostname/$appname/status.php?jobid=$jobid&email=$email");
-		?>
+
+	?>
 	</section>
 
 

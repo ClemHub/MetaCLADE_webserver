f8b49a208e1699cfb1107901655533b45f8024df
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-03-31 16:39:32 +0200

    small form update

--- MyCLADE/includes/function.js
+++ MyCLADE/includes/function.js
@@ -23,13 +23,15 @@ function validateFasta(fasta) {
 function validatePFAM(pfam_list){
 	var pfam_exp = /^PF\d{5}$/;
 	pfam_list = pfam_list.split(',');
-	alert(pfam_list);
-	alert(pfam_list.length);
-	valid = true;
+	list_len = pfam_list.length;
+	if(list_len > 10){
+		valid = false;}
+	else{
+		valid = true;
 		for(var pfam in pfam_list){
 			if(!pfam_exp.test(pfam_list[pfam])){
 				valid = false;
-				break;}}
+				break;}}}
 	return valid;}
 
 function large_form_submission(){

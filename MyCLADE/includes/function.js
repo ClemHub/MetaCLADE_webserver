19aa60c4621f304fc887d404ca96c5b251aeaed9
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-01 13:44:21 +0200

    small form update

--- MyCLADE/includes/function.js
+++ MyCLADE/includes/function.js
@@ -192,16 +192,13 @@ function filter_table(){
 	document.getElementById(filter).checked=true;
 	table = document.getElementById("result");
 	tr = table.getElementsByTagName("tr");
-	for (i = 1; i < tr.length; i++) {
-		// Hide the row initially.
-		tr[i].style.display = "none";
-		td = tr[i].getElementsByTagName("td");
-		for (var j = 0; j < td.length; j++) {
-		cell = tr[i].getElementsByTagName("td")[j];
-		if (cell) {
-			if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+	for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 			tr[i].style.display = "";
-			break;}}}}}
+		} else {
+			tr[i].style.display = "none";}}}}
 
 function uncheckAll(divid) {
 	var checks = document.querySelectorAll('#' + divid + ' input[type="checkbox"]');

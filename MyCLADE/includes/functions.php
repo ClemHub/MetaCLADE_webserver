e1c42313a6b07b55de46b247f6b5972e2c2eba13
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-06 13:59:41 +0200

    configuration

--- MyCLADE/includes/functions.php
+++ MyCLADE/includes/functions.php
@@ -25,7 +25,7 @@ function submit($jobid, $email){
 	echo 'e-value: '.$e-value.' <br>';
 	$dama = $_SESSION['dama'];
 	echo 'DAMA: '.$dama.' <br>';
-	$args = escapeshellarg('-i '.$appurl.'/MyCLADE/jobs/'.$jobid.'/data.fa')." ".escapeshellarg('-N '.$jobid)." ".escapeshellarg('-e '.$e_value)." ".escapeshellarg('-W http://localhost:1234/MetaCLADE_webserver/MyCLADE/jobs/'.$jobid)." ".escapeshellarg('--sge ')." ".escapeshellarg('--pe smp ')." ".escapeshellarg('-j '.$nb_jobs)." ";
+	$args = escapeshellarg('-i '.$appurl.'/MyCLADE/jobs/'.$jobid.'/data.fa')." ".escapeshellarg('-N '.$jobid)." ".escapeshellarg('-e '.$e_value)." ".escapeshellarg('-W http://localhost`/MetaCLADE_webserver/MyCLADE/jobs/'.$jobid)." ".escapeshellarg('--sge ')." ".escapeshellarg('--pe smp ')." ".escapeshellarg('-j '.$nb_jobs)." ";
 	if($dama == 'true'){
 		$DAMA_evalue = $_SESSION['DAMA-evalue'];
 		echo 'DAMA e-value: '.$DAMA_evalue.' <br>';	

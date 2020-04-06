dc67e1c156f744518ceba1a441de54b71ced97fa
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-06 11:43:37 +0200

    rearrangements

--- MyCLADE/cookies.php
+++ MyCLADE/cookies.php
@@ -8,5 +8,17 @@
 		$_SESSION['pfam_domains'] = $_POST['pfam_domains'];
 		$form = $_GET['form'];
 
+		echo $_SESSION['dama'];
+		echo '<br>';
+		echo $_SESSION['evalue'];
+		echo '<br>';
+		echo $_SESSION['DAMA-evalue'];
+		echo '<br>';
+		echo $_SESSION['sequences'];
+		echo '<br>';
+		echo $_SESSION['pfam_domains'];
+		echo '<br>';
+		echo $form;
+
 		header("location: http://localhost/MetaCLADE_webserver/MyCLADE/submit.php?form=".$form);
 ?>

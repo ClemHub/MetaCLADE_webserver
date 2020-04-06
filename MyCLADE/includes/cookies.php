0e748bfaeb7879c29e4a8a14585a086ed5612b80
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-06 11:56:20 +0200

    rearrangements

--- MyCLADE/includes/cookies.php
+++ MyCLADE/includes/cookies.php
@@ -1,7 +1,3 @@
 <?php
 		session_start();
-		$_SESSION = array();
-		$_SESSION['dama'] = $_POST['dama'];
-		$_SESSION['evalue'] = $_POST['evalue_nb'];
-		$_SESSION['DAMA-evalue'] = $_POST['dama_evalue_range'];
 ?>

9a4630e54ddf05f86eb76f89b93516356758173d
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-06 13:10:25 +0200

    configuration

--- MyCLADE/includes/footer.php
+++ MyCLADE/includes/footer.php
@@ -8,13 +8,13 @@
 			<a href="http://iscd.sorbonne-universite.fr/" target="_blank"><img id="ISCD-logo" class="img-responsive" src="http://localhost:8888/MetaCLADE_webserver/server_images/ISCD-logo.png" alt="" style="height: 90%; width:400px"></a> -->
 
 
-			<a href="https://www.sorbonne-universite.fr/" target="_blank"><img id="UPMC-logo" class="img-responsive" src="http://localhost/MetaCLADE_webserver/server_images/Sorbonne-logo.png" alt="" style="height: 90%; "></a>
+			<a href="https://www.sorbonne-universite.fr/" target="_blank"><img id="UPMC-logo" class="img-responsive" src="http://localhost:1234/MetaCLADE_webserver/server_images/Sorbonne-logo.png" alt="" style="height: 90%; "></a>
 
-			<a href="http://www.cnrs.fr" target="_blank"><img id="CNRS-logo" class="img-responsive" src="http://localhost/MetaCLADE_webserver/server_images/CNRS-logo.png" alt="" style="height: 90%; "></a>
+			<a href="http://www.cnrs.fr" target="_blank"><img id="CNRS-logo" class="img-responsive" src="http://localhost:1234//MetaCLADE_webserver/server_images/CNRS-logo.png" alt="" style="height: 90%; "></a>
 
-			<a href="http://www.lcqb.upmc.fr/" target="_blank"><img id="LCQB-logo" class="img-responsive" src="http://localhost/MetaCLADE_webserver/server_images/LCQB-logo.jpg" alt="" style="height: 90%; "></a>
+			<a href="http://www.lcqb.upmc.fr/" target="_blank"><img id="LCQB-logo" class="img-responsive" src="http://localhost:1234//MetaCLADE_webserver/server_images/LCQB-logo.jpg" alt="" style="height: 90%; "></a>
 
-			<a href="http://iscd.sorbonne-universite.fr/" target="_blank"><img id="ISCD-logo" class="img-responsive" src="http://localhost/MetaCLADE_webserver/server_images/ISCD-logo.png" alt="" style="height: 90%; width:400px"></a>
+			<a href="http://iscd.sorbonne-universite.fr/" target="_blank"><img id="ISCD-logo" class="img-responsive" src="http://localhost:1234//MetaCLADE_webserver/server_images/ISCD-logo.png" alt="" style="height: 90%; width:400px"></a>
 
 	</footer>
 	<script src="./includes/function.js"></script>

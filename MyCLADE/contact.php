658de1523b9cf90ab5c58ae0d4121400619c3407
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-07 14:07:38 +0200

    configuration

--- MyCLADE/contact.php
+++ MyCLADE/contact.php
@@ -1,90 +1,63 @@
-<?php include("./includes/header.php"); ?>
-<?php include("./includes/menu.php"); ?>
+<?php 
+include('./includes/header.php'); 
+include('./includes/menu.php'); 
 
-	<section id = 'contact'>
-	<h2> Contact </h2>
+	echo "<section id = 'contact'>";
+	echo "<h2> Contact </h2>";
 
-	<div id='contact_container'>
-		<fieldset class = 'contact_fs'><legend class = "contact_title">Alessandra Carbonne</legend>
-		<!-- <img id="AC" class="contact_img" src="http://localhost:8888/MetaCLADE_webserver/server_images/AC-picture.png" alt="" > -->
-		<img id="AC" class="contact_img" src="http://localhost:1234/MetaCLADE_webserver/server_images/AC-picture.png" alt="" >
-		<p class="contact_descr">
-		PI - Analytical Genomics
-		</p>
-		<p class="contact_descr">
-		Sorbonne Université
-		</p>	
-		<div class='social_media_div'>
-		<a href="http://www.ihes.fr/~carbone/" target='_blank' class="fas fa-globe"></a>
-		<a href="https://twitter.com/acarbone16" target='_blank' class="fab fa-twitter"></a>
-		<a href="mailto:alessandra.carbone@lip6.fr" class="fas fa-at"></a>
-		</div>
-		</fieldset>
+	echo "<div id='contact_container'>";
+		echo "<fieldset class = 'contact_fs'><legend class = 'contact_title'>Alessandra Carbonne</legend>";
+		echo "<img id='AC' class='contact_img' src=".$appurl."/server_images/AC-picture.png' alt='' >";
+		echo "<p class='contact_descr'>";
+		echo "PI - Analytical Genomics</p>";
+		echo "<p class='contact_descr'>Sorbonne Université</p>";	
+		echo "<div class='social_media_div'>";
+		echo "<a href='http://www.ihes.fr/~carbone/' target='_blank' class='fas fa-globe'></a>";
+		echo "<a href='https://twitter.com/acarbone16' target='_blank' class='fab fa-twitter'></a>";
+		echo "<a href='mailto:alessandra.carbone@lip6.fr' class='fas fa-at'></a></div></fieldset>";
 
-		<fieldset class = 'contact_fs'><legend class = "contact_title">Riccardo Vicedomini</legend>
-		<!-- <img id="AC" class="contact_img" src="http://localhost:8888/MetaCLADE_webserver/server_images/AC-picture.png" alt="" > -->
-		<img id="RV" class="contact_img" src="http://localhost:1234/MetaCLADE_webserver/server_images/AC-picture.png" alt="" >
-		<p class="contact_descr">
-		PI - Analytical Genomics
-		</p>
-		<p class="contact_descr">
-		Sorbonne Université
-		</p>	
-		<div class='social_media_div'>
-		<a href="#" target='_blank' class="fas fa-globe"></a>
-		<a href="#" target='_blank' class="fab fa-twitter"></a>
-		<a href="#" target='_blank' class="fas fa-at"></a>
-		</div>
-		</fieldset>
 
-		<fieldset class = 'contact_fs'><legend class = "contact_title">Personne 3</legend>
-		<!-- <img id="P3" class="contact_img" src="http://localhost:8888/MetaCLADE_webserver/server_images/AC-picture.png" alt="" > -->
-		<img id="AC" class="contact_img" src="http://localhost:1234/MetaCLADE_webserver/server_images/AC-picture.png" alt="" >
-		<p class="contact_descr">
-		PI - Analytical Genomics
-		</p>
-		<p class="contact_descr">
-		Sorbonne Université
-		</p>	
-		<div class='social_media_div'>
-		<a href="#" class="fas fa-globe"></a>
-		<a href="#" class="fab fa-twitter"></a>
-		<a href="#" class="fas fa-at"></a>
-		</div>
-		</fieldset>
+		echo "<fieldset class = 'contact_fs'><legend class = 'contact_title'>Riccardo Vicedomini</legend>";
+		echo "<img id='RV' class='contact_img' src=".$appurl."/server_images/AC-picture.png' alt='' >";
+		echo "<p class='contact_descr'>";
+		echo "PI - Analytical Genomics</p>";
+		echo "<p class='contact_descr'>Sorbonne Université</p>";	
+		echo "<div class='social_media_div'>";
+		echo "<a href='#' target='_blank' class='fas fa-globe'></a>";
+		echo "<a href='#' target='_blank' class='fab fa-twitter'></a>";
+		echo "<a href='#' class='fas fa-at'></a></div></fieldset>";
 
-		<fieldset class = 'contact_fs'><legend class = "contact_title">Personne 4</legend>
-		<!-- <img id="P4" class="contact_img" src="http://localhost:8888/MetaCLADE_webserver/server_images/AC-picture.png" alt="" > -->
-		<img id="AC" class="contact_img" src="http://localhost:1234/MetaCLADE_webserver/server_images/AC-picture.png" alt="" >
-		<p class="contact_descr">
-		PI - Analytical Genomics
-		</p>
-		<p class="contact_descr">
-		Sorbonne Université
-		</p>	
-		<div class='social_media_div'>
-		<a href="#" class="fas fa-globe"></a>
-		<a href="#" class="fab fa-twitter"></a>
-		<a href="#" class="fas fa-at"></a>
-		</div>
-		</fieldset>
+		echo "<fieldset class = 'contact_fs'><legend class = 'contact_title'>Riccardo Vicedomini</legend>";
+		echo "<img id='RV' class='contact_img' src=".$appurl."/server_images/AC-picture.png' alt='' >";
+		echo "<p class='contact_descr'>";
+		echo "PI - Analytical Genomics</p>";
+		echo "<p class='contact_descr'>Sorbonne Université</p>";	
+		echo "<div class='social_media_div'>";
+		echo "<a href='#' target='_blank' class='fas fa-globe'></a>";
+		echo "<a href='#' target='_blank' class='fab fa-twitter'></a>";
+		echo "<a href='#' class='fas fa-at'></a></div></fieldset>";
 
-		<fieldset class = 'contact_fs'><legend class = "contact_title">Personne 5</legend>
-		<!-- <img id="P5" class="contact_img" src="http://localhost:8888/MetaCLADE_webserver/server_images/AC-picture.png" alt="" > -->
-		<img id="AC" class="contact_img" src="http://localhost:1234/MetaCLADE_webserver/server_images/AC-picture.png" alt="" >
-		<p class="contact_descr">
-		PI - Analytical Genomics
-		</p>
-		<p class="contact_descr">
-		Sorbonne Université
-		</p>	
-		<div class='social_media_div'>
-		<a href="#" class="fas fa-globe"></a>
-		<a href="#" class="fab fa-twitter"></a>
-		<a href="#" class="fas fa-at"></a>
-		</div>
-		</fieldset>
-	</div>
-	</section>
+		echo "<fieldset class = 'contact_fs'><legend class = 'contact_title'>Riccardo Vicedomini</legend>";
+		echo "<img id='RV' class='contact_img' src=".$appurl."/server_images/AC-picture.png' alt='' >";
+		echo "<p class='contact_descr'>";
+		echo "PI - Analytical Genomics</p>";
+		echo "<p class='contact_descr'>Sorbonne Université</p>";	
+		echo "<div class='social_media_div'>";
+		echo "<a href='#' target='_blank' class='fas fa-globe'></a>";
+		echo "<a href='#' target='_blank' class='fab fa-twitter'></a>";
+		echo "<a href='#' class='fas fa-at'></a></div></fieldset>";
 
-<?php include("./includes/footer.php"); ?>
+		echo "<fieldset class = 'contact_fs'><legend class = 'contact_title'>Riccardo Vicedomini</legend>";
+		echo "<img id='RV' class='contact_img' src=".$appurl."/server_images/AC-picture.png' alt='' >";
+		echo "<p class='contact_descr'>";
+		echo "PI - Analytical Genomics</p>";
+		echo "<p class='contact_descr'>Sorbonne Université</p>";	
+		echo "<div class='social_media_div'>";
+		echo "<a href='#' target='_blank' class='fas fa-globe'></a>";
+		echo "<a href='#' target='_blank' class='fab fa-twitter'></a>";
+		echo "<a href='#' class='fas fa-at'></a></div></fieldset>";
+	
+	echo "</div></section>";
+
+include('./includes/footer.php'); 
+?>

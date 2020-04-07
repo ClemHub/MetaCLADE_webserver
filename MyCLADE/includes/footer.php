aa1929df5974cd5f75fee764765f6825f66cea74
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-07 14:24:59 +0200

    configuration

--- MyCLADE/includes/footer.php
+++ MyCLADE/includes/footer.php
@@ -12,7 +12,7 @@
 
 			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='http://localhost:1234/MetaCLADE_webserver/server_images/CNRS-logo.png' alt='' style='height: 90%; '></a>";
 
-			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 90%; '></a>";
+			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='http://".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 90%; '></a>";
 
 			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='http://localhost:1234/MetaCLADE_webserver/server_images/ISCD-logo.png' alt='' style='height: 90%; width:400px'></a>";
 ?>

4ca8f1ec1ddaa9943c0f77a40e0bdcb4f5d3d378
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-07 14:35:23 +0200

    configuration

--- MyCLADE/includes/footer.php
+++ MyCLADE/includes/footer.php
@@ -1,13 +1,7 @@
-	<footer>
-			<!-- <a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='http://localhost:8888/MetaCLADE_webserver/server_images/sorbonne-universite-LOGO_SU_HORIZ_SIGNATURE_RVB_PNG.png' alt='' style='height: 90%; '></a>
-
-			<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='http://localhost:8888/MetaCLADE_webserver/server_images/CNRS-logo.png' alt='' style='height: 90%; '></a>
-
-			<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='http://localhost:8888/MetaCLADE_webserver/server_images/LCQB-logo.jpg' alt='' style='height: 90%; '></a>
-
-			<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='http://localhost:8888/MetaCLADE_webserver/server_images/ISCD-logo.png' alt='' style='height: 90%; width:400px'></a> -->
-
+	
 <?php
+			echo "<footer>";
+
 			echo "<a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='http://".$appurl."/server_images/Sorbonne-logo.png' alt='' style='height: 90%; '></a>";
 
 			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='http://".$appurl."/server_images/CNRS-logo.png' alt='' style='height: 90%; '></a>";
@@ -15,8 +9,10 @@
 			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='http://".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 90%; '></a>";
 
 			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='http://".$appurl."/server_images/ISCD-logo.png' alt='' style='height: 90%; width:400px'></a>";
+
+			echo "</footer>";
+
 ?>
-	</footer>
 	<script src='./includes/function.js'></script>
 </div>
 </body>
--- MyCLADE/includes/header.php
+++ MyCLADE/includes/header.php
@@ -1,57 +1,49 @@
 <?php
-include("./includes/cookies.php");
+include('./includes/cookies.php');
 include('./includes/configure.php');
 include('./includes/logfunctions.php');
 include('./includes/functions.php');
+
+
+echo "<!DOCTYPE html>";
+echo "<html lang='en'>";
+echo "<head>";
+	echo "<meta charset='utf-8' />";
+	echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'> ";
+	echo "<link rel='stylesheet' type='text/css'  href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/css_style/style.css'>";
+	echo "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>";
+	echo "<script src='https://kit.fontawesome.com/a076d05399.js'></script>";
+	echo "<title>Home</title></head>";
+
+
+	echo "<body>";
+		echo "<div id='container'>";
+			echo "<div id='header-menu'>";
+				echo "<header><h1>MyCLADE</h1></header>";
+				echo "<div class='topnav' id='myTopnav'>";
+					echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/home.php' class = 'active'>Home</a>";
+				echo "<div class='dropdown'>";
+				echo "<button class='dropbtn'>Tools";
+				echo "<i class='fa fa-caret-down'></i>";
+				echo "</button>";
+				echo "<div class='dropdown-content'>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/small_annotation.php'>Few domains annotation</a>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/large_annotation.php'>All domains annotation</a>";
+				echo "</div>";
+				echo "</div>";
+				echo "<div class='dropdown'>";
+				echo "<button class='dropbtn'>Help";
+				echo "<i class='fa fa-caret-down'></i>";
+				echo "</button>";
+				echo "<div class='dropdown-content'>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#input'>Input</a>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#output'>Output</a>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#method'>Method</a>";
+				echo "</div>";
+				echo "</div>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/references.php'>References</a>";
+				echo "<a href='http://localhost:1234/MetaCLADE_webserver/MyCLADE/contact.php'>Contact</a>";
+				echo "<a href='javascript:void(0);' class='icon' onclick='nav_function()'>&#9776;</a>";
+				echo "</div>";
+				echo "</div>";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<!-- <link rel="stylesheet" type="text/css"  href="http://localhost:8888/MetaCLADE_webserver/css_style/style.css"> -->
-	<link rel="stylesheet" type="text/css"  href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/css_style/style.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
-	<title>Home</title>
-</head>
-
-<body>
-<div id='container'>
-	<div id="header-menu">
-		<header><h1>MyCLADE</h1></header>
-		<div class="topnav" id="myTopnav">
-			<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/home.php" class = 'active'>Home</a> -->
-			<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/home.php" class = 'active'>Home</a>
-			<div class='dropdown'>
-				<button class="dropbtn">Tools
-					<i class="fa fa-caret-down"></i>
-				</button>
-				<div class="dropdown-content">
-					<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/small_annotation.php">Few domains annotation</a>
-					<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/small_annotation.php">Few domains annotation</a> -->
-					<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/large_annotation.php">All domains annotation</a>
-					<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/large_annotation.php">All domains annotation</a> -->
-				</div>
-			</div>
-			<div class='dropdown'>
-				<button class="dropbtn">Help
-					<i class="fa fa-caret-down"></i>
-				</button>
-				<div class="dropdown-content">
-					<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/help.php#input">Input</a> -->
-					<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#input">Input</a>
-					<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/help.php#output">Output</a> -->
-					<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#output">Output</a>
-					<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/help.php#method">Method</a> -->
-					<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/help.php#method">Method</a>
-				</div>
-			</div>
-			<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/references.php">References</a> -->
-			<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/references.php">References</a>
-			<!-- <a href="http://localhost:8888/MetaCLADE_webserver/MyCLADE/contact.php">Contact</a> -->
-			<a href="http://localhost:1234/MetaCLADE_webserver/MyCLADE/contact.php">Contact</a>
-			<a href="javascript:void(0);" class="icon" onclick="nav_function()">&#9776;</a>
-		</div>
-	</div>

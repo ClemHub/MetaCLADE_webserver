d5a1a7f20aee65355b98ddf960761a1a51adcb19
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-11 21:08:14 +0200

    configuration

--- MyCLADE/includes/header.php
+++ MyCLADE/includes/header.php
@@ -10,7 +10,7 @@ echo "<html lang='en'>";
 echo "<head>";
 	echo "<meta charset='utf-8' />";
 	echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'> ";
-	echo "<link rel='stylesheet' type='text/css'  href='".$appurl."/MetaCLADE_webserver/MyCLADE/css_style/style.css'>";
+	echo "<link rel='stylesheet' type='text/css'  href='".$appurl."/MyCLADE/css_style/style.css'>";
 	echo "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>";
 	echo "<script src='https://kit.fontawesome.com/a076d05399.js'></script>";
 	echo "<title>MetaCLADE</title></head>";

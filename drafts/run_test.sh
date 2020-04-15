77571d3f62da61142df31def06c8b3b76fb738aa
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-15 13:07:56 +0200

    server test

--- drafts/run_test.sh
+++ drafts/run_test.sh
@@ -11,7 +11,7 @@
 #$ -cwd
 #Merge the standard out and standard error to one file
 #$ -j y
-/home/blachon/Documents/Tools/metaclade2 -i ./test.fasta -N testDataSet -d PF00875,PF03441,PF03167,PF12546 -W ./ --sge --pe smp -j 2 -t 2
+./home/blachon/Documents/Tools/metaclade2/metaclade2 -i ./test.fasta -N testDataSet -d PF00875,PF03441,PF03167,PF12546 -W ./ --sge --pe smp -j 2 -t 2
 # Send mail at submission and completion of script
 #$ -m be
 #$ -M clemenceblachon@gmail.com

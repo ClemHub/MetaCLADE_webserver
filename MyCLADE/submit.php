ad0dd18727d197d2d1b81597cc487990018f9fca
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-15 17:33:27 +0200

    server test

--- MyCLADE/submit.php
+++ MyCLADE/submit.php
@@ -11,8 +11,8 @@ include("./includes/header.php");
 
 		$jobid = generateRandomString();
 		echo 'Your job ID is:'.$jobid,'<br>';
-		mkdir($appurl.'/MyCLADE/jobs/'.$jobid, 0777, true);
-		if(!mkdir($appurl.'/MyCLADE/jobs/'.$jobid, 0777, true)){
+		mkdir('/var/www/html/MetaCLADE_webserver/MyCLADE/jobs/cGp5cIwmxq/', 0777, true);
+		if(!mkdir('/var/www/html/MetaCLADE_webserver/MyCLADE/jobs/cGp5cIwmxq/', 0777, true)){
 			echo "The directory has not been created<br>";
 		}
 		$msg = submit($jobid, $email);

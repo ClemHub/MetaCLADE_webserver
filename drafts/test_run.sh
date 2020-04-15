cafe370cf8a26e8a801b658d7c2fa4513ccb9fee
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-15 12:26:32 +0200

    server test

--- drafts/test_run.sh
+++ drafts/test_run.sh
@@ -1,2 +1,27 @@
-#!/bin/bash
-metaclade2 -i ./test.fasta -N testDataSet -d PF00875,PF03441,PF03167,PF12546 -W ./ --sge --pe smp -j 2 -t 2
+#!/bin/sh
+#
+# Usage: sleeper.sh [time]]
+#        default for time is 60 seconds
+
+# -- our name ---
+#$ -N Sleeper1
+#$ -S /bin/sh
+# Make sure that the .e and .o file arrive in the
+# working directory
+#$ -cwd
+#Merge the standard out and standard error to one file
+#$ -j y
+/bin/echo Here I am: `hostname`. Sleeping now at: `date`
+/bin/echo Running on host: `hostname`.
+/bin/echo In directory: `pwd`
+/bin/echo Starting on: `date`
+# Send mail at submission and completion of script
+#$ -m be
+#$ -M deadline@kronos
+time=60
+if [ $# -ge 1 ]; then
+   time=$1
+fi
+sleep $time
+
+echo Now it is: `date`

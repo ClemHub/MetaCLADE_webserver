43c3e790ff8ea36dff242a62a731bcb53446060c
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-16 22:04:37 +0200

    program test

--- MyCLADE/includes/functions.php
+++ MyCLADE/includes/functions.php
@@ -30,8 +30,8 @@ function submit($jobid, $email){
 	//echo 'DAMA: '.$dama.' <br>';
 	$nb_jobs = 2;
 	$input_file = escapeshellarg($approot.'/MyCLADE/jobs/'.$jobid.'/data.fa');
-	$args = escapeshellarg('-N '.$jobid)." ".escapeshellarg(' -i '.$approot.'/MyCLADE/jobs/'.$jobid.'/data.fa ')." ".escapeshellarg('-e '.$e_value.' ')." ".escapeshellarg('-W '.$approot.'/MyCLADE/jobs/'.$jobid.' ')." ".escapeshellarg('-j '.$nb_jobs.' ')." ";
-	$args = '-i '.$input_file.' -e '.$e_value.' -W '.$approot.'/MyCLADE/jobs/'.$jobid.' -j '.$nb_jobs.' ';
+	$args = '-N '.$jobid.escapeshellarg(' -i '.$approot.'/MyCLADE/jobs/'.$jobid.'/data.fa ')." ".escapeshellarg('-e '.$e_value.' ')." ".escapeshellarg('-W '.$approot.'/MyCLADE/jobs/'.$jobid.' ')." ".escapeshellarg('-j '.$nb_jobs.' ')." ";
+	//$args = '-i '.$input_file.' -e '.$e_value.' -W '.$approot.'/MyCLADE/jobs/'.$jobid.' -j '.$nb_jobs.' ';
 	if($dama == 'true'){
 		$DAMA_evalue = $_SESSION['DAMA-evalue'];
 		//echo 'DAMA e-value: '.$DAMA_evalue.' <br>';	

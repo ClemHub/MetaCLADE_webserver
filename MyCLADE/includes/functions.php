36ec55f049320fd02a6891c8a3a97a4d754fe2b0
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-16 15:14:15 +0200

    program test

--- MyCLADE/includes/functions.php
+++ MyCLADE/includes/functions.php
@@ -39,12 +39,14 @@ function submit($jobid, $email){
 		$args = $args .escapeshellarg('-d '.$pfam);}
 	//ARGS is the list of arguments you have extracted from your form. Only this is escaped because it is the only things given by the user. 
 	//Sublit your job
-	//echo "command is launch<br>";
+	echo "command is launch<br>";
 	$command="qsub -wd ".$approot."/MyCLADE/jobs/".$jobid."/ -N $jobid ".$approot."/drafts/run_test.sh";
-	//echo 'command: '.$command.'<br>';
+	echo 'command: '.$command.'<br>';
+	echo '<br>';
 	$output = shell_exec("$command");
-	//echo "after shell_exec<br>";
-	//echo 'output: '.$output.'<br>';
+	echo "after shell_exec<br>";
+	echo 'output: '.$output.'<br>';
+	echo '<br>';
 	//$link="$appurl/status.php?jobid=$jobid"; #status.php is a page that show he status of your job
 	$msg="<strong>Your job has been correctly submitted</strong><br><br>";
 	//$msg= $msg . "You can follow job progress as well as downloading the results going to <a target=_blank href=$link> $link </a><br>";

a7bcf4b4af100455f840bfda4aaeebcc8280ce1e
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-16 17:49:04 +0200

    program test

--- MyCLADE/run.sh
+++ MyCLADE/run.sh
@@ -80,10 +80,10 @@ while [ -n "${1}" ]; do
 done
 
 if [$a = 'true']
-then 
-   /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -d "$MCLADE_DOMLIST" -e "$MCLADE_EVALUECUTOFF" -a -E "$MCLADE_EVALUECUTCONF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp 
+then
+   /bin/echo /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -d "$MCLADE_DOMLIST" -e "$MCLADE_EVALUECUTOFF" -a -E "$MCLADE_EVALUECUTCONF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp 
 else 
-   /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -e "$MCLADE_EVALUECUTOFF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp
+   /bin/echo /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -e "$MCLADE_EVALUECUTOFF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp
 fi
 
 

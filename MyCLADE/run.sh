3d63b8ca58bc8326c6af813919b88f93c26c33f4
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-16 19:31:30 +0200

    program test

--- MyCLADE/run.sh
+++ MyCLADE/run.sh
@@ -83,7 +83,7 @@ if [$a = 'true']
 then
    /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -d "$MCLADE_DOMLIST" -e "$MCLADE_EVALUECUTOFF" -a -E "$MCLADE_EVALUECUTCONF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp -t 2 
 else 
-   /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -e "$MCLADE_EVALUECUTOFF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp -t 2
+   /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -d "$MCLADE_DOMLIST" -e "$MCLADE_EVALUECUTOFF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp -t 2
 fi
 
 

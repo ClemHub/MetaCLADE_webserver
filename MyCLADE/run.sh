32d5ea42796e9ef7fa150b4f7feed0ea14483e03
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-17 12:31:40 +0200

    status

--- MyCLADE/run.sh
+++ MyCLADE/run.sh
@@ -78,8 +78,7 @@ while [ -n "${1}" ]; do
     esac
     shift
 done
-/bin/echo "$MCLADE_DOMLIST"
-if 
+
 if [$a = 'true'] and ["$MCLADE_DOMLIST"] != ""
 then
    /home/blachon/Documents/Tools/metaclade2/metaclade2 -i "$INPUT_FASTA" -N "$MCLADE_JOBNAME" -d "$MCLADE_DOMLIST" -e "$MCLADE_EVALUECUTOFF" -a -E "$MCLADE_EVALUECUTCONF" -W "$MCLADE_WORKDIR" -j 2 --sge --pe smp -t 2 

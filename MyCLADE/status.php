bd0cbf506398e8847814cbcfdb92af4b84de19b0
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-21 22:42:08 +0200

    submit

--- MyCLADE/status.php
+++ MyCLADE/status.php
@@ -20,7 +20,8 @@ include("./includes/header.php");
 		if($output){
 			echo 'here<br>';
 			foreach($output as $file){
-				echo $file;
+				$file = end(explode("/", $file));
+				echo $file.'<br>';
 				if(preg_match('[a-zA-Z0-9]+\.e[0-9]+', $file)){
 					$e_file = $file;}
 				else if(preg_match('[a-zA-Z0-9]+\.o[0-9]+', $file)){

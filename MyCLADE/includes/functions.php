ddfe09b192948f3b75a3b3e86063b6ce4e069b63
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-21 14:55:15 +0200

    results modification

--- MyCLADE/includes/functions.php
+++ MyCLADE/includes/functions.php
@@ -27,30 +27,22 @@ function submit($jobid, $email){
 	$dama = $_SESSION['dama'];
 	$form = $_GET["form"];
 	$nb_jobs = 2;
-	//$args = escapeshellarg('-i '.$approot.'/MyCLADE/jobs/'.$jobid.'/data.fa').' '.escapeshellarg('-N '.$jobid).' '.escapeshellarg('-e '.$e_value)." ".escapeshellarg('-W '.$approot.'/MyCLADE/jobs/'.$jobid)." ".escapeshellarg('-j '.$nb_jobs)." ";
 	$args = "-i ".escapeshellarg("$approot/MyCLADE/jobs/".$jobid."/data.fa")." -N ".escapeshellarg($jobid)."  -e ".escapeshellarg($e_value)."  -W ".escapeshellarg("$approot/MyCLADE/jobs/".$jobid)."  -j ".escapeshellarg($nb_jobs);
-	//$args = '-i '.$approot.'/MyCLADE/jobs/'.$jobid.'/data.fa -N '.$jobid.' -e '.$e_value.' -W '.$approot.'/MyCLADE/jobs/'.$jobid.' -j '.$nb_jobs.' ';
 	if($dama == 'true'){
 		$DAMA_evalue = $_SESSION['DAMA-evalue'];	
 		$args = $args." -a -E ".escapeshellarg($DAMA_evalue);}
-		//$args = $args . '-a -E ' . $DAMA_evalue . ' ';}
 	if($_GET['form']=='small'){
 		$pfam = $_SESSION['pfam_domains'];	
 		$args = $args." -d ".escapeshellarg($pfam);}
-		//$args = $args . '-d '.$pfam;}
-	//ARGS is the list of arguments you have extracted from your form. Only this is escaped because it is the only things given by the user. 
 	//Submit your job
 	if ($form == 'small'){
 		$command="qsub -wd ".$approot."/MyCLADE/jobs/".$jobid."/ -N $jobid ".$approot."/MyCLADE/run_small.sh ".$args;}
 	else if($form == 'large'){
 		$command="qsub -wd ".$approot."/MyCLADE/jobs/".$jobid."/ -N $jobid ".$approot."/MyCLADE/run_large.sh ".$args;}
-	//echo 'command: '.$command.'<br>';
 	$output = shell_exec("$command");
-	//echo 'output: '.$output.'<br>';
-	//echo '<br>';
-	//$link="$appurl/status.php?jobid=$jobid"; #status.php is a page that show he status of your job
+	$link = "$hostname/$appname/MyCLADE/status.php?form=".$form."&job_id=".$job_id."&email=".$email; 
 	$msg="<strong>Your job has been correctly submitted</strong><br><br>";
-	//$msg= $msg . "You can follow job progress as well as downloading the results going to <a target=_blank href=$link> $link </a><br>";
+	$msg= $msg . "You can follow job progress as well as downloading the results going to <a target=_blank href=$link> $link </a><br>";
 	$msg= $msg . "<br>Your data will be removed one month after the end of the job.<br>";
 	$msg= $msg . "The job will be stopped if longer than 48 hours.<br>";
 	$msg= $msg . "If you need some help, contact the web developer (".$webdevel.").<br>";

8be2823aefb9fd026a4789215134fbecc49bcdb1
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-21 22:03:21 +0200

    submit

--- MyCLADE/status.php
+++ MyCLADE/status.php
@@ -13,8 +13,8 @@ include("./includes/header.php");
         echo "You can save the link to access the results later thanks to this link:<br>"; 
         echo "<a href=$hostname/$appname/MyCLADE/status.php?form=".$form."&job_id=".$job_id."&email=".$email.">$hostname/$appname/MyCLADE/status.php?form=".$form."&job_id=".$job_id."</a><br>";
         echo "<br>This page will be refreshed every 10 seconds<br>";
-        echo approot."/MyCLADE/jobs/".$job_id."/".$job_id.".[eo][0-9]+<br>";
-        $output =  glob(approot."/MyCLADE/jobs/".$job_id."/".$job_id.".*");
+        //echo approot."/MyCLADE/jobs/".$job_id."/".$job_id.".[eo][0-9]+<br>";
+        $output =  glob(approot."/MyCLADE/jobs/".$job_id."/".$job_id.".[eo][0-9]+");
         print_r($output);
         if(file_exists($approot."/MyCLADE/jobs/".$job_id."/".$job_id."/results/3_arch/".$job_id.".arch.txt")){
             header("location: $hostname/$appname/MyCLADE/results.php?form=".$form."&job_id=".$job_id."&email=".$email);

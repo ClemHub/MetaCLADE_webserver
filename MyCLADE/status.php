5eb8ed9c9d22157033987321c2a27cfcb0b6747c
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-24 13:05:05 +0200

    parameters storage

--- MyCLADE/status.php
+++ MyCLADE/status.php
@@ -55,11 +55,7 @@ include("./includes/header.php");
 		$DAMA_eval = true;
 		echo "<ul><br><strong>Your job parameters:</strong><br>";
 		//echo "<br><strong>Your job parameters:</strong><br>";
-		foreach($parameters as $name => $value){
-			echo $name.': '.$value.'<br>';
-			if($name == "DAMA" and $value == 'false'){
-				echo 'oook';
-			}}
+		print_r($parameters);
 			/*if($name == "DAMA" and $value == 'false'){
 				$DAMA_eval = false;
 				echo "<li>".$name.": ".$value."</li>";}

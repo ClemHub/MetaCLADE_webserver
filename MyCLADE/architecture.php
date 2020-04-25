069476a02baa4ce1e4fc869a6dad6252404f7085
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-25 13:09:50 +0200

    database

--- MyCLADE/architecture.php
+++ MyCLADE/architecture.php
@@ -113,14 +113,14 @@
 		$link_id = 'http://pfam.xfam.org/family/' . $pfam;
 		echo "<tr><td rowspan=".$nb."><a class = 'table_link' href=" . $link_id . " target='_blank'>".$pfam."</a></td>";
 		$request = $db->query("SELECT * FROM GO_terms WHERE Domain='".$pfam."'");
-		$request = $request->fetchArray();
-		echo "<td rowspan=".$nb.">" . $row['Family']."</td>";
 		$nb = $request->numRows();
 		if ($nb > 0) {
 			$i = 0;
 			while($row = $request->fetchArray()){
 				if($i==0){
-					echo "<td rowspan=".$nb.">" . $row['Family']."</td>";}
+					$row2 = $db->query("SELECT DISTINCT PFAM32.Family FROM PFAM32 WHERE PFAM32.PFAM_acc_nb='".$pfam."'");
+					$row2 = $row2->fetchArray();
+					echo "<td rowspan=".$nb.">" . $row2['Family']."</td>";}
 				echo "<td>" . $row['GO_term'] . '</td></tr>';
 				$i++;
 			}}

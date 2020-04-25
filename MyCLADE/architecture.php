8760fde8731bfc1539ed6284eb7e4ada242f15da
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-25 12:24:50 +0200

    database

--- MyCLADE/architecture.php
+++ MyCLADE/architecture.php
@@ -58,8 +58,8 @@
 	<div class='info'>
 	<input type='button' class='bouton_info' value='Results informations:' onclick='close_open_info(this);' />
 	<div class='contenu_info'>
-	print_r($pfam_list);
 	<div class='table_container' id='architecture_data'>
+	print_r($pfam_list);
 	<table id='data_table'>
 	<thead>
 		<tr>

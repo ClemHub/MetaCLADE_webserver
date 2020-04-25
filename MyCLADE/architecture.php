814729dbb70c049613b8a0ec214e0e69bc0ad563
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-25 13:43:28 +0200

    database

--- MyCLADE/architecture.php
+++ MyCLADE/architecture.php
@@ -116,6 +116,7 @@
 		$pfam_row = $pfam_row->fetchArray();
 		echo "<td>" . $pfam_row['Family']."</td>";
 		$request =  $db->query("SELECT DISTINCT PFAM32.Family FROM PFAM32 WHERE PFAM32.PFAM_acc_nb='".$pfam."'");
+		$request = $request->fetchArray();
 		echo "<td>" . $request."</td>";
 		if($request){
 			$nb = $request->numRows();

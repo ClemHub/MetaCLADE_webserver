9b265b255f0337278adecaf8800a65c79f522279
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-26 19:47:23 +0200

    database

--- MyCLADE/includes/function.js
+++ MyCLADE/includes/function.js
@@ -78,13 +78,13 @@ function small_form_submission() {
 function fill_exemple_form(form){
 	if(form == 'small'){
 		document.small_annotation_form.pfam_domains.value = "PF00875,PF03441,PF03167,PF12546"
-		fetch('/MyCLADE/fasta_file/example.fasta')
+		fetch('MetaCLADE_webserver/MyCLADE/fasta_file/example.fasta')
 		.then(response => response.text())
 		.then((data) => {document.small_annotation_form.sequences.value = data })
 		document.small_annotation_form.action = 'session.php?form=small_example';
 		document.getElementById("pfam_domains").disabled = true;}
 	else{
-		fetch('/MyCLADE/fasta_file/example.fasta')
+		fetch('MetaCLADE_webserver/MyCLADE/fasta_file/example.fasta')
 		.then(response => response.text())
 		.then((data) => {document.large_annotation_form.sequences.value = data })
 		document.large_annotation_form.action = 'session.php?form=large_example'}

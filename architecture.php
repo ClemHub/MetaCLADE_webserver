253d58192620e991e3e30ca53297339dd1e49738
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-28 17:51:58 +0200

    results

--- architecture.php
+++ architecture.php
@@ -150,6 +150,7 @@
 	</div>
 	</div>
 	</section>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
 	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
   	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
   	<script>

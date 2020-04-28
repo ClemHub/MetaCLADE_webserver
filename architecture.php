201b900e79b21fde02b3c07c9504474faea4e326
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-28 17:48:21 +0200

    results

--- architecture.php
+++ architecture.php
@@ -154,7 +154,7 @@
   	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
   	<script>
 	$(document).ready(function() {
-    $('#example').DataTable( {
+    $('#data_table').DataTable( {
         "order": [[ 3, "desc" ]]
     } );
 } );

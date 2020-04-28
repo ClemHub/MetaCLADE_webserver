317a6d824e5827f47c575a61c8ac18b1c1de317f
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-28 12:03:01 +0200

    results

--- includes/function.js
+++ includes/function.js
@@ -204,6 +204,30 @@ function filter_table(){
 		} else {
 			tr[i].style.display = "none";}}}}
 
+$(document).ready(function(){
+	$('#results').after('<div id="nav"></div>');
+	var rowsShown = 2;
+	var rowsTotal = $('#results tbody tr').length;
+	var numPages = rowsTotal/rowsShown;
+	for(i = 0;i < numPages;i++) {
+		var pageNum = i + 1;
+		$('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+	}
+	$('#results tbody tr').hide();
+	$('#results tbody tr').slice(0, rowsShown).show();
+	$('#nav a:first').addClass('active');
+	$('#nav a').bind('click', function(){
+
+		$('#nav a').removeClass('active');
+		$(this).addClass('active');
+		var currPage = $(this).attr('rel');
+		var startItem = currPage * rowsShown;
+		var endItem = startItem + rowsShown;
+		$('#results tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+		css('display','table-row').animate({opacity:1}, 300);
+	});
+});
+
 // function filter_arch_table(col_nb, id){
 //   var input, filter, table, tr, td, i, txtValue;
 //   input = document.getElementById(id);

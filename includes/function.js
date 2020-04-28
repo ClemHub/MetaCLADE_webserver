6b79d1bb55bc99330462b23fbebf40e04265aa5e
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-28 13:47:53 +0200

    results

--- includes/function.js
+++ includes/function.js
@@ -1,23 +1,24 @@
-function validate_one_seq(seq) {
-	if (!seq) {return false;}
+function validate_one_seq(seq){
+	if(!seq){
+		return false;}
 	seq = seq.trim();
 	var lines = seq.split('\n');
-
-	if (seq[0] == '>') {lines.splice(0, 1);}
-	else{return false;}
+	if(seq[0] == '>'){
+		lines.splice(0, 1);}
+	else{
+		return false;}
 	seq = lines.join('').trim();
-
-	if (!seq) {return false;}
-
+	if (!seq){
+		return false;}
 	return /^[ACDEFGHIKLMNPQRSTUVWY\s]+$/i.test(seq);}
 
-function validateFasta(fasta) {
+function validateFasta(fasta){
 	var seq = fasta.split(/(?=\>)/);
 	for(s in seq){
 		if(seq[s]){
 			valid = validate_one_seq(seq[s])
-			if(!valid)
-				{break}}}
+			if(!valid){
+				break}}}
 	return valid}
 
 function validatePFAM(pfam_list){
@@ -45,39 +46,38 @@ function large_form_submission(){
 		valid = false}
 	return valid;}
 
-function small_form_submission() {
+function small_form_submission(){
 	var seq =  document.small_annotation_form.sequences.value.trim();
 	var pfam_domains = document.small_annotation_form.pfam_domains.value.trim();
 	var valid = true;
 	if(seq!="" && pfam_domains==""){
-		alert("Please, enter a list of PFAM domains and do not enter more than 10 domains.")
-		valid = false}
+		alert("Please, enter a list of PFAM domains and do not enter more than 10 domains.");
+		valid = false;}
 	else if(seq=="" && pfam_domains!=""){
 		if(!validatePFAM(pfam_domains)){
-			alert("\tPlease:\n-Enter a set of sequences or browse a fasta file\n-Respect the PFAM domain format and do not enter more than 10 domains.")
-			valid = false}
+			alert("\tPlease:\n-Enter a set of sequences or browse a fasta file\n-Respect the PFAM domain format and do not enter more than 10 domains.");
+			valid = false;}
 		else{
-			alert("\tPlease, enter:\n-A set of sequences or browse a fasta file.")
-			valid = false}}
+			alert("\tPlease, enter:\n-A set of sequences or browse a fasta file.");
+			valid = false;}}
 	else if(seq=="" && pfam_domains==""){
-		alert("\tPlease, enter:\n-A set of sequences manually or through a fasta file\n-A list of 10 PFAM domains maximum.")
-		valid = false}
+		alert("\tPlease, enter:\n-A set of sequences manually or through a fasta file\n-A list of 10 PFAM domains maximum.");
+		valid = false;}
 	else if(seq !="" && pfam_domains!=""){
 		if(seq != "" && !validateFasta(seq) && !validatePFAM(pfam_domains)){
-			alert("\tPlease, respect:\n-The PFAM domain format and do not enter more than 10 domains.\n-The fasta format")
-			valid = false}
+			alert("\tPlease, respect:\n-The PFAM domain format and do not enter more than 10 domains.\n-The fasta format");
+			valid = false;}
 		else if(seq != "" && !validateFasta(seq) && validatePFAM(pfam_domains)){
-			alert("\tPlease, respect:\n-The Fasta format")
-			valid = false}
+			alert("\tPlease, respect:\n-The Fasta format");
+			valid = false;}
 		else if(seq!="" && validateFasta(seq) && !validatePFAM(pfam_domains)){
-			alert("\tPlease, respect:\n-The PFAM domain format and do not enter more than 10 domains.")
-			valid = false}
-		}
-	return valid}
+			alert("\tPlease, respect:\n-The PFAM domain format and do not enter more than 10 domains.");
+			valid = false;}}
+	return valid;}
 
 function fill_exemple_form(form){
 	if(form == 'small'){
-		document.small_annotation_form.pfam_domains.value = "PF00875,PF03441,PF03167,PF12546"
+		document.small_annotation_form.pfam_domains.value = "PF00875,PF03441,PF03167,PF12546";
 		fetch('/MetaCLADE_webserver/data/example.fasta')
 		.then(response => response.text())
 		.then((data) => {document.small_annotation_form.sequences.value = data })
@@ -102,13 +102,10 @@ function fill_exemple_form(form){
 function reset_btn(form){
 	if(form == 'small'){
 		document.small_annotation_form.action = 'cookies.php?form=small';
-		document.getElementById("pfam_domains").disabled = false;
-	}
+		document.getElementById("pfam_domains").disabled = false;}
 	else{
-		document.large_annotation_form.action = 'cookies.php?form=large';
-	}
+		document.large_annotation_form.action = 'cookies.php?form=large';}
 	document.getElementById("sequences").disabled = false;
-	
 	document.getElementById("evalue_nb").disabled = false;
 	document.getElementById("evalue_range").disabled = false;
 	document.getElementById("dama_evalue_nb").disabled = false;
@@ -116,7 +113,7 @@ function reset_btn(form){
 	document.getElementById("email").disabled = false;
 	document.getElementById("show_dama").style.display = "none";}
 
-function nav_function() {
+function nav_function(){
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {
 		x.className += " responsive";}
@@ -127,30 +124,29 @@ function close_open_info(bouton) {
 	var divContenu = bouton.nextSibling;
 	if(divContenu.nodeType == 3) {divContenu = divContenu.nextSibling;}
 	if(divContenu.style.display == 'block') {
-		divContenu.style.display = 'none';
-	}
+		divContenu.style.display = 'none';}
 	else {
 		divContenu.style.display = 'block';}}
 
-function ShowHideDiv() {
+function ShowHideDiv(){
 	var yes_btn = document.getElementById("yes_btn");
 	var evalue_dama = document.getElementById("show_dama");
 	evalue_dama.style.display = yes_btn.checked ? "block" : "none";}
 
-function ran_col() {
+function ran_col(){
 	var color = '#';
 	var letters = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0'];
 	color += letters[Math.floor(Math.random() * letters.length)];
 	document.getElementById('posts').style.background = color;}
 
-function showTooltip(evt, text) {
+function showTooltip(evt, text){
 	let tooltip = document.getElementById("tooltip");
 	tooltip.innerHTML = text;
 	tooltip.style.display = "block";
 	tooltip.style.left = evt.pageX + 10 + 'px';
-	tooltip.style.top = evt.pageY + 10 + 'px';  }
+	tooltip.style.top = evt.pageY + 10 + 'px';}
 
-function hideTooltip() {
+function hideTooltip(){
 	var tooltip = document.getElementById("tooltip");
 	tooltip.style.display = "none";}
 
@@ -188,11 +184,11 @@ function filter_table(){
 	tr = table.getElementsByTagName("tr");
 	for (i = 0; i < tr.length; i++) {
 		td = tr[i].getElementsByTagName("td")[1];
-		if (td) {
-		if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-			tr[i].style.display = "";
-		} else {
-			tr[i].style.display = "none";}}}}
+		if (td){
+			if(td.innerHTML.toUpperCase().indexOf(filter) > -1){
+				tr[i].style.display = "";}
+			else{
+				tr[i].style.display = "none";}}}}
 
 // function filter_arch_table(col_nb, id){
 //   var input, filter, table, tr, td, i, txtValue;
@@ -224,9 +220,8 @@ function reset_table(id){
 	var table, tr, i;
 	table = document.getElementById(id);
 	tr = table.getElementsByTagName("tr");
-	for (i = 0; i < tr.length; i++) {
-		tr[i].style.display = "";
-	}}
+	for (i = 0; i < tr.length; i++){
+		tr[i].style.display = "";}}
 
 function filter_all_domains(){
 	var filter, checkboxes, table, tr, td, i, f;
@@ -241,7 +236,7 @@ function filter_all_domains(){
 		if (td) {
 			for (f= 0; f < filter.length; f++){
 				if (td.innerHTML.toUpperCase().indexOf(filter[f]) > -1) {
-					tr[i].style.display = "";
-				} else {
+					tr[i].style.display = "";}
+				else{
 					tr[i].style.display = "none";
 					break}}}}}

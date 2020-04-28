0b1ffa8cf4cd3aa062873396b346307fe27b8ee4
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-28 17:43:02 +0200

    results

--- architecture.php
+++ architecture.php
@@ -164,14 +164,9 @@
 	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
   	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
   	<script>
-  $(function(){
-    $("#example").dataTable();
-  })
-  </script>
-	<script>
-	$(document).ready(function(){
-		$('#data_table').DataTable();
-	});
+	$(function(){
+		$("#data_table").dataTable();
+	})
 	</script>
 
 <?php include("./includes/footer.php"); ?>

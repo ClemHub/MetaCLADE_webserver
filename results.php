1ac361117e663023cd8c1fbf3f54afa09a5db385
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-29 11:38:33 +0200

    results

--- results.php
+++ results.php
@@ -90,12 +90,11 @@ include("./includes/header.php");
 			<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
 			<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
 			<script>
-			$(document).ready(function() {
 			$('#result').DataTable( 
 				{"order": [[ 2, "desc" ]],
 				"pagingType": "full_numbers",
 				"pageLength": 2}
-			)});
+			);
 
 			</script>
 		<div class='table_container'>

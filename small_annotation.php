368b2dc29b0761a0ce929536a9e731d5bcea55c5
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-29 12:27:47 +0200

    results

--- small_annotation.php
+++ small_annotation.php
@@ -6,7 +6,7 @@
 		<form name="small_annotation_form" method = POST action="submit.php?form=small" enctype="multipart/form-data" onsubmit="return small_form_submission()">
 			<fieldset class='form_fs'><legend><h4>Input data:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See examples in the Help section for the expected format.<br>And do not enter more than 10 domains.</span></span></h4></legend>
 			<div id='pfam_container'>
-			<label for="pfam_domains">PFAM accession number:</label><br/>
+			<label for="pfam_domains">PFAM accession number: <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Do not enter more than 10 domains.</span></span></label><br/>
 			<textarea name="pfam_domains" id = "pfam_domains" rows='10' placeholder="Example:&#10;PF04523,PF06584,PF06325" autofocus></textarea>
 			
 			</div>

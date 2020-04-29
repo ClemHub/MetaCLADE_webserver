498858c4744afc44b4a204ad7cbfbb8665d1ab9c
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-29 13:53:08 +0200

    results

--- results.php
+++ results.php
@@ -88,7 +88,7 @@ include("./includes/header.php");
 		?>
 			<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
 			<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-			<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+			<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 			<script>
 	$(document).ready(function() {
 		$('#result').DataTable( {

2d6ac79e8938f90834651bc96ce347e4fe79e5cd
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-30 13:08:22 +0200

    results

--- results.php
+++ results.php
@@ -148,7 +148,7 @@ include("./includes/header.php");
 			}
 			echo "</select></th>";
 			?>
-			<th class='table_header'><input id='max' type='number'/></th>
+			<th class='table_header'><input id='max' value = '1' type='number'/></th>
 			</tr>
 		</tfoot>		
 		<tbody>

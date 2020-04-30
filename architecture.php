05017deb297a6d5a835f5cff736040d911ed1a9a
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-30 18:14:20 +0200

    results

--- architecture.php
+++ architecture.php
@@ -286,11 +286,11 @@
 			$nb ++;
 			array_push($rows, $row);}
 		if($nb == 0){
-			echo "<tr><td rowspan=".$nb."><a class = 'table_link' href=" . $link_id . " target='_blank'>".$data."</a></td>";
-			echo "<td rowspan=".$nb.">" . $pfam_row['Family']."</td>";
-			echo "<td rowspan=".$nb.">" . $Clan_acc_nb."</td>";
-			echo "<td rowspan=".$nb.">" . $Clan."</td>";
-			echo "<td rowspan=".$nb.">NA</td></tr>";}
+			echo "<tr><td><a class = 'table_link' href=" . $link_id . " target='_blank'>".$data."</a></td>";
+			echo "<td>" . $pfam_row['Family']."</td>";
+			echo "<td>" . $Clan_acc_nb."</td>";
+			echo "<td>" . $Clan."</td>";
+			echo "<td>NA</td></tr>";}
 		else{
 			$i = 0;
 			foreach($rows as $row){

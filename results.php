1590fb24d7d3f5a04a49da138b000e1e5f69b6aa
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-04-30 13:07:06 +0200

    results

--- results.php
+++ results.php
@@ -95,8 +95,8 @@ include("./includes/header.php");
 
 		$.fn.dataTable.ext.search.push(
 			function( settings, data, dataIndex ) {
-				var max = parseInt( $('#max').val(), 10);
-				var e_value = parseFloat( data[2] ) || 0; // use data for the age column
+				var max = Number( $('#max').val(), 10);
+				var e_value = Number( data[2] ) || 0; // use data for the age column
 				if ((isNaN(max)) || (e_value <= max)){
 					return true;}
 				return false;

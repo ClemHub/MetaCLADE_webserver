f203d8040beccf1fa94324b07a44f7989f3ff870
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-06 11:27:03 +0200

    results

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@ echo "<head>";
 	echo "<body>";
 		echo "<div id='container'>";
 			echo "<div id='header-menu'>";
-				echo "<header><img id='UPMC-logo' class='img-responsive' src='".$appurl."/server_images/header.png' alt='' style='height: 50%; '></header>";
+				echo "<header></header>";
 				echo "<div class='topnav' id='myTopnav'>";
 					echo "<a href='".$appurl."/index.php' class = 'active'>Home</a>";
 				echo "<div class='dropdown'>";

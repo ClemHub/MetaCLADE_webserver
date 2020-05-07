71aaa1fa45be240c9837f52fa549ca35d90caeed
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-07 17:25:17 +0200

    architecture update

--- architecture.php
+++ architecture.php
@@ -123,9 +123,9 @@
 			echo "<option value='".$species."'>".$species."</option>";}
 		echo "</select></th>";	
 		?>
-		<th class='table_header'><input id='e-value_max' type='text' placeholder='E-value max'/></th>
-		<th class='table_header'><input id='bitscore_min' type='text' placeholder='Bitscore min'/></th>
-		<th class='table_header'><input id='acc_min' type='text' placeholder='Accuracy min'/></th>
+		<th class='table_header'><input id='e-value_max' type='text' placeholder='E-value max' style='width:100px'/></th>
+		<th class='table_header'><input id='bitscore_min' type='text' placeholder='Bitscore min' style='width:100px'/></th>
+		<th class='table_header'><input id='acc_min' type='text' placeholder='Accuracy min' style='width:100px'/></th>
 		</tr>
 	</tfoot>	
 	<?php

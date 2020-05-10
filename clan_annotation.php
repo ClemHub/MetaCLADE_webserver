6c0e64e2d07d788f9e34c2884731af028c710331
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-10 12:43:55 +0200

    new tool

--- clan_annotation.php
+++ clan_annotation.php
@@ -1,19 +1,23 @@
 <?php include("./includes/header.php"); ?>
 
 	<section class = 'tools'>
-	<h2> Annotation of small datasets of sequences with all Pfam domains<br><span id = 'subtitle'>Searching for domains</span></h2>
-	
-		<form name="large_annotation_form" method = "post" action="submit.php?form=large"  enctype="multipart/form-data" onsubmit="return large_form_submission()">
-			<fieldset class='form_fs'><legend><h4>Input data:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See examples in the Help section for the expected format.</span></span></h4></legend>
-			<div class='seq_container'>
-			<label for="sequences">Sequences in Fasta format:</label><br/>
-			<textarea name="sequences" id = "sequences" rows='10' placeholder="Example:&#10;>SeqID_1&#10;sequence_1&#10;>SeqID_2&#10;sequence_2&#10;" autofocus></textarea><br/>
-			<label for="fasta_file">Upload a Fasta file:</label>
-			<input type="file" id="fasta_file" name="fasta_file""/>
-			</div>
-			<div id='error_message'></div>
-			</fieldset>
+		<h2> Annotation of a clan in large and small datasets of sequences <br><span id = 'subtitle'>Searching for domains</span></h2>
+		
+		<form name="small_annotation_form" method = POST action="submit.php?form=small" enctype="multipart/form-data" onsubmit="return small_form_submission()">
+			<fieldset class='form_fs'><legend><h4>Input data:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See examples in the Help section for the expected format.<br>And do not enter more than 10 domains.</span></span></h4></legend>
+			<div id='pfam_container'>
+			<label for="pfam_domains">Clan: <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Do not enter more than 10 domains.</span></span></label><br/>
+			<textarea name="pfam_domains" id = "pfam_domains" rows='10' placeholder="Example:&#10;PF04523,PF06584,PF06325" autofocus></textarea>
 			
+			</div>
+
+			<div id='seq_container'>
+			<label for="sequences">Sequences in Fasta format:</label><br/>
+			<textarea name="sequences" id = "sequences" rows='10' placeholder="Example:&#10;>SeqID_1&#10;sequence_1&#10;>SeqID_2&#10;sequence_2&#10;"></textarea><br/>
+			<label for="fasta_file">Upload a Fasta file:</label>
+			<input type="file" id="fasta_file" name="fasta_file"/>
+			</div>
+			</fieldset>
 			<fieldset class='form_fs'><legend><h4>Parameters:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>The parameters are defined with default values but you can modify them.</span></span></h4></legend>
 			<div class = 'metaclade_e-value'>
 			<label for='evalue_range'>E-value threshold for MetaCLADE:<br/></label>
@@ -34,25 +38,24 @@
 
 				<label for='dama_evalue_range'>E-value threshold for DAMA:<br/></label>
 					<input type="range" id='dama_evalue_range' name="dama_evalue_range" min='0' max="1" value="1e-10" step='1e-10' oninput="this.form.dama_evalue_nb.value=this.value" />
-					<input type="number" name="dama_evalue_nb" id='dama_evalue_nb' min='0' max="1" value="1e-10" step='1e-10' oninput="this.form.dama_evalue_range.value=this.value" />
+					<input type="number" id ='dama_evalue_nb' name="dama_evalue_nb" min='0' max="1" value="1e-10" step='1e-10' oninput="this.form.dama_evalue_range.value=this.value" />
 				<span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>E-value set to 1e<sup>-10</sup> by default. We advice you to avoid an E-value higher than 1e<sup>-5</sup>.<br/> Results with E-value greater than or equal to 1 are automatically filtered out.</span></span>
 			</div>
 			</fieldset>
-
 			<fieldset class='form_fs'><legend><h4>E-mail adress:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Optional and used to send you job details.</span></span></h4></legend>
+
 			<div id = 'email_address'>
 			<input type="email" name="email" id="email" pattern=".+@.+\..+" placeholder = 'Optionnal'>
 			</div>
 			</fieldset>
-			
 			<div id='submission'>
 			<br/>
-			<input class='btn' type="submit" value="Search" name = "submit"/><input class='btn' type="reset" value="Reset" onclick="reset_btn('large')"/>
-			<span class='tooltip'><input class='btn' type="button" value="Example " onclick="fill_exemple_form('large')"/><span class='tooltiptext'>It uploads a precalculated example. Results will be filtered with your parameter choice. </span></span>
-			</div>			
-		</form>	
-		</section>
-		<script type="text/javascript">
+			<input class='btn' type="submit" value="Search" name = "submit"/><input class='btn' type="reset" value="Reset" onclick="reset_btn('small')"/>
+			<span class='tooltip'><input class='btn' type="button" value="Example " onclick="fill_exemple_form('small')"/><span class='tooltiptext'>It uploads a precalculated example. Results will be filtered with your parameter choice.</span></span>
+			</div>
+		</form>
+	</section>
+	<script type="text/javascript">
 		var fileInput = document.querySelector('#fasta_file');
 		fileInput.addEventListener('change', function() {
 		var reader = new FileReader();
@@ -60,5 +63,5 @@
 			var txt = reader.result
 			document.getElementById('sequences').value = txt;});
 		reader.readAsText(fileInput.files[0]);});
-		</script>
+	</script>
 <?php include("./includes/footer.php"); ?>

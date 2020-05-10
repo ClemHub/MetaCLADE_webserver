ed26f5c4061896738ad76ef46fe5e6893215dc5d
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-10 12:27:06 +0200

    new tool

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,13 @@
 <?php
 			echo "<footer>";
 
-			echo "<a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='".$appurl."/server_images/Sorbonne-logo.png' alt='' style='height: 90%; '></a>";
+			echo "<a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='".$appurl."/server_images/Sorbonne-logo.png' alt='' style='height: 80%; '></a>";
 
-			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='".$appurl."/server_images/CNRS-logo.png' alt='' style='height: 90%; '></a>";
+			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='".$appurl."/server_images/CNRS-logo.png' alt='' style='height: 80%; '></a>";
 
-			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 90%; '></a>";
+			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 80%; '></a>";
 
-			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='".$appurl."/server_images/ISCD-logo.png' alt='' style='height: 90%; width:320px'></a>";
+			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='".$appurl."/server_images/ISCD-logo.png' alt='' style='height: 80%; width:300px'></a>";
 
 			echo "</footer>";
 

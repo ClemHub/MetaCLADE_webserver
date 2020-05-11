6b35b2bd3a9e7ede385c71974ab454b3a1a4db50
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-11 13:06:18 +0200

    new tool

--- clan_annotation.php
+++ clan_annotation.php
@@ -8,6 +8,11 @@
 			<div class="autocomplete" id='clan_container'>
 			<label for="clan">Clan: <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Enter the name of the clan.</span></span></label><br/>
 			<input id="clan" type="text" name="clan" placeholder="CL00001">
+			<input type='button' onclick='clan_selection()'/>
+			</div>
+			<div id='pfam_container'>
+			<label for="pfam_domains">PFAM accession number: <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Do not enter more than 10 domains.</span></span></label><br/>
+			<textarea name="pfam_domains" id = "pfam_domains" rows='10' placeholder="Example:&#10;PF04523,PF06584,PF06325" autofocus></textarea>
 			</div>
 			<div id='seq_container'>
 			<label for="sequences">Sequences in Fasta format:</label><br/>

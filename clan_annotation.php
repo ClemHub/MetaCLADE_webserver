67579b9f66361df68de07537889160314050c612
Clémence Blachon <clemence@macbook-pro-de-clemence.home>
2020-05-11 12:26:00 +0200

    new tool

--- clan_annotation.php
+++ clan_annotation.php
@@ -80,7 +80,6 @@ function autocomplete(inp, arr) {
 		a = document.createElement("DIV");
 		a.setAttribute("id", this.id + "autocomplete-list");
 		a.setAttribute("class", "autocomplete-items");
-		a.setAttribute("style", "height=200px, overflow=scroll");
 		a.setAttribute("style", "");
 		/*append the DIV element as a child of the autocomplete container:*/
 		this.parentNode.appendChild(a);

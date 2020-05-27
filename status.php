5e7ffbddecac55dd65a516820c657f59b4f02671
Clémence Blachon <clemence@MBP-de-Clemence-1.home>
2020-05-27 13:08:21 +0200

    error

--- status.php
+++ status.php
@@ -52,7 +52,19 @@ include("./includes/header.php");
 			header("refresh: 10");}
 
 		$parameters = read_parameters_file($approot."/jobs/".$job_id."/parameters.txt");
-		$DAMA_eval = true;
+		echo $parameters['DAMA'];
+		echo '<br>';
+		if($parameters['DAMA'] == 'false'){
+			echo "DAMA is a string";
+		} 
+		else if($parameters['DAMA'] == false){
+			echo "DAMA is a boolean";
+		}
+		else{
+			echo "DAMA is none of that";
+		}
+		echo '<br>';
+
 		echo "<ul><br><strong>Your job parameters:</strong><br>";
 		foreach($parameters as $name => $value){
 			if($name != "" and $value != "" and $name != "Email"){

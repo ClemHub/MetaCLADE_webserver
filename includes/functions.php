ca43ba54ca92d2de904d775deaafe57e17f7fc51
Clémence Blachon <clemence@MBP-de-Clemence-1.home>
2020-05-27 13:15:43 +0200

    error

--- includes/functions.php
+++ includes/functions.php
@@ -32,7 +32,7 @@ function submit($job_id, $email){
 	$e_value = $parameters['E-value'];
 	$dama = $parameters['DAMA'];
 	$args = "-i ".escapeshellarg("$approot/jobs/".$job_id."/data.fa")." -N ".escapeshellarg($job_id)."  -e ".escapeshellarg($e_value)."  -W ".escapeshellarg("$approot/jobs/");
-	if($dama == 'true'){
+	if($dama == true){
 		$DAMA_evalue = $parameters['DAMA e-value'];	
 		$overlappingAA = $parameters['overlappingAA'];
 		$overlappingMaxDomain = $parameters['overlappingMaxDomain'];

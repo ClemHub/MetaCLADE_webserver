5338b4aad835eeb106ea6868e6041051440f805f
Clémence Blachon <clemence@macbook-pro.home>
2020-10-13 23:34:18 +0200

    ddProb

--- architecture.php
+++ architecture.php
@@ -63,7 +63,7 @@
 			echo "<g><a xlink:href='http://pfam.xfam.org/family/".$pfam."' target='_blank'>";
 			//echo "<text class='rect_text' x='". $scaled_start ."%' y='30' style='font-size:15px; font-size-adjust: 0.5; fill:white; font-weight:bold; mix-blend-mode: exclusion;' >".$pfam."</text>";
 			echo "<rect class='domain_rect' x='".$scaled_start."%' y='10' width='". $width ."%' height='30' style=' fill:".$color."; fill-opacity:0.7; stroke-width:1; stroke:3'>";
-			echo "<title>PFAM Acc Number: ".$pfam."\nFamily: ".$row['Family']."\n\nPosition: ".$start."-".$stop." (".$nb_aa."aa)\n\nClan Acc Number: ".$row['Clan_acc_nb']."\nClan: ".$row['Clan']."\n\nModel species: ".$model_species."\n\nE-value: ".$exploded_line[9]."\nbitscore: ".$exploded_line[10]."\nAccuracy: ".$exploded_line[11]."</title></rect></a></g>";
+			echo "<title>PFAM Acc Number: ".$pfam."\nFamily: ".$row['Family']."\n\nPosition: ".$start."-".$stop." (".$nb_aa."aa)\n\nClan Acc Number: ".$row['Clan_acc_nb']."\nClan: ".$row['Clan']."\n\nModel species: ".$model_species."\n\nE-value: ".$exploded_line[9]."\nbitscore: ".$exploded_line[10]."\ddProb: ".$exploded_line[11]."</title></rect></a></g>";
 			array_push($pfam_name, $pfam);
 			array_push($pfam_fam, $row['Family']);
 			array_push($pfam_clan_nb, $row['Clan_acc_nb']);
@@ -109,7 +109,7 @@
 		<th class='table_header' style='max-width:190px'>Model species</th>
 		<th class='table_header' style='max-width:170px'>E-Value</th>
 		<th class='table_header' style='max-width:170px'>Bitscore</th>
-		<th class='table_header' style='max-width:170px'>Accuracy</th>
+		<th class='table_header' style='max-width:170px'>ddProb</th>
 		</tr>
 	</thead>
 	<tfoot>

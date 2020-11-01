2e02b82aef05302f33a317067c5259843725f423
Clémence Blachon <clemence@macbook-pro.home>
2020-11-01 16:02:19 +0100

    space

--- clan_annotation.php
+++ clan_annotation.php
@@ -71,7 +71,7 @@
 			<div id='submission'>
 			<br/>
 			<input class='btn' type="submit" value="Search" name = "submit"/>&emsp;<input class='btn' type="reset" value="Reset" onclick="reset_btn('clan')"/>&emsp;
-			<span class='tooltip'><input class='btn' type="button" value="Example " onclick="fill_exemple_form('clan')"/><span class='tooltiptext'>It uploads a precalculated example, you can choose wheter or not to use DAMA. All the other parameters are defined with default values.</span></span>
+			<span class='tooltip'><input class='btn' type="button" value="Example" onclick="fill_exemple_form('clan')"/><span class='tooltiptext'>It uploads a precalculated example, you can choose wheter or not to use DAMA. All the other parameters are defined with default values.</span></span>
 			</div>
 		</form>
 	</section>

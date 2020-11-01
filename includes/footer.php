20b6fab47896c1f36a36e333ad83b7a92e74ebb4
Clémence Blachon <clemence@macbook-pro.home>
2020-11-01 13:08:05 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,16 +4,15 @@
 			
 			echo "<p> LCQB Lab - This website is free and open to all users and there is no login requirement </p>";
 
+			echo "<a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='".$appurl."/server_images/Sorbonne-logo.png' alt='' style='height: 60%; '></a>";
+
+			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='".$appurl."/server_images/CNRS-logo.png' alt='' style='height: 60%; '></a>";
+
+			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 60%; '></a>";
+
+			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='".$appurl."/server_images/ISCD-logo.png' alt='' style='height: 60%; width: 250px'></a>";
+
 			echo "</footer>";
-			
-			echo "<a href='https://www.sorbonne-universite.fr/' target='_blank'><img id='UPMC-logo' class='img-responsive' src='".$appurl."/server_images/Sorbonne-logo.png' alt='' style='height: 80%; '></a>";
-
-			echo "<a href='http://www.cnrs.fr' target='_blank'><img id='CNRS-logo' class='img-responsive' src='".$appurl."/server_images/CNRS-logo.png' alt='' style='height: 80%; '></a>";
-
-			echo "<a href='http://www.lcqb.upmc.fr/' target='_blank'><img id='LCQB-logo' class='img-responsive' src='".$appurl."/server_images/LCQB-logo.jpg' alt='' style='height: 80%; '></a>";
-
-			echo "<a href='http://iscd.sorbonne-universite.fr/' target='_blank'><img id='ISCD-logo' class='img-responsive' src='".$appurl."/server_images/ISCD-logo.png' alt='' style='height: 80%; width: 250px'></a>";
-
 			
 
 ?>

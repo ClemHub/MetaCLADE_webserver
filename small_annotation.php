e631216e2975fc6b3bbc99127c9efb3a13e78b19
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-13 16:08:42 +0100

    job name

--- small_annotation.php
+++ small_annotation.php
@@ -66,7 +66,7 @@
 			<fieldset class='form_fs'><legend><h4>Job name:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Optional and used to store your job details.</span></span></h4></legend>
 
 			<div id = 'job_name_div'>
-			<input type="text" id="job_name" placeholder = 'Optional'>
+			<input type="text" id="job_name" name="job_name" placeholder = 'Optional'>
 			</div>
 			</fieldset>
 			<fieldset class='form_fs'><legend><h4>E-mail address:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>Optional and used to send your job details.</span></span></h4></legend>

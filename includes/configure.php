e634b452dcf6b24e9d22907e78c4898e8fcccc71
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-13 22:18:34 +0100

    mail

--- includes/configure.php
+++ includes/configure.php
@@ -1,5 +1,5 @@
 <?php 
-$appname  = 'MyCLADE';
+$appname  = 'MetaCLADE_webserver';
 $appsroot = '/var/www/html'; #It is "/appsdata" on production whatever you want on development. Anyway, has to be accessible to apache
 $approot  = $appsroot.'/'.$appname;  #Directory where the app is hosted. It has to be accessible from apache user
 //$hostname = 'http://localhost:1234'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development

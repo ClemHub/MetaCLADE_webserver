ed29c9d91368706dbf55a825d10eaf2b67c6b1f0
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-13 15:25:23 +0100

    correction

--- architecture.php
+++ architecture.php
@@ -3,7 +3,7 @@
 	$form = $_GET['form'];
 	if($form != 'visualization'){
 		$job_id = $_GET['job_id'];
-		$name_file = $approot."/jobs/".$job_id."/".$job_id.".arch.txt";}
+		$name_file = $approot."/jobs/".$job_id."/".$job_id.".arch.tsv";}
 	else{
 		$job_id = $_GET['job_id'];
 		$name_file = $approot."/jobs/".$job_id."/visualization.txt";}

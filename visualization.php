fe4cd333b52762a0b1376f2b2d3b8479a505c667
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-13 17:34:11 +0100

    job id visualization

--- visualization.php
+++ visualization.php
@@ -7,12 +7,12 @@
 			<div>
 			To visualize annotations made by this webserver, the format must be unchanged.
 			</div>
-			<fieldset class='form_fs'><legend><h4> Or results ID:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>The randomly generated ID during submission.</span></span></h4></legend>
+			<fieldset class='form_fs'><legend><h4> Results ID:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>The randomly generated ID during submission.</span></span></h4></legend>
 			<div id = 'job_name_div'>
 			<input type="text" id="job_ID" name="job_ID" placeholder = 'Optional'>
 			</div>
 			</fieldset>
-			<fieldset class='form_fs'><legend><h4>Input file:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See Help for expected format.</span></span></h4></legend>
+			<fieldset class='form_fs'><legend><h4>Or input file:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See Help for expected format.</span></span></h4></legend>
 			<div id='seq_container'>
 			<label for="fasta_file">Upload an annotation file:</label>
 			<textarea name="sequences" id = "sequences" rows='10' placeholder="Example: tr|A0A072NB93|A0A072NB93_9DEIO  12      141     766     PF00875 A6FVP5_9RHOB_1-137      1       129     130     1.4e-35 111.3   0.97    Roseobacter sp. AzwK-3b"></textarea><br/>

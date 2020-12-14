4695524ea0e85867a678e3e80a7c175a5be2bfbf
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-14 11:25:43 +0100

    test

--- visualization.php
+++ visualization.php
@@ -3,7 +3,7 @@
 	<section class = 'tools'>
 		<h2>Visualization of an annotation file</h2>
 		
-		<form autocomplete="off" name="visualization_form" method = POST action="submit.php?form=visualization" enctype="multipart/form-data">
+		<form autocomplete="off" name="visualization_form_ID" method = POST action="submit.php?form=visualization_jobID" enctype="multipart/form-data">
 			<div>
 			To visualize annotations made by this webserver, the format must be unchanged (don't put the header details of your result).
 			</div>
@@ -17,12 +17,12 @@
 			<input class='btn' type="submit" value="Search" name = "submit"/><input class='btn' type="reset" value="Reset" onclick="reset_btn('visualisation')"/>
 			</div>
 		</form>
-		<form autocomplete="off" name="visualization_form" method = POST action="submit.php?form=visualization" enctype="multipart/form-data" onsubmit="return visualization_form_submission()">
+		<form autocomplete="off" name="visualization_form_file" method = POST action="submit.php?form=visualization_file" enctype="multipart/form-data" onsubmit="return visualization_form_submission()">
 			<fieldset class='form_fs'><legend><h4>Or input file:  <span class='tooltip'><i class="far fa-question-circle"></i><span class='tooltiptext'>See Help for expected format.</span></span></h4></legend>
 			<div id='seq_container'>
-			<label for="fasta_file">Upload an annotation file:</label>
+			<label for="annotation_file">Upload an annotation file:</label>
 			<textarea name="sequences" id = "sequences" rows='10' placeholder="Example: tr|A0A072NB93|A0A072NB93_9DEIO  12      141     766     PF00875 A6FVP5_9RHOB_1-137      1       129     130     1.4e-35 111.3   0.97    Roseobacter sp. AzwK-3b"></textarea><br/>
-			<input type="file" id="fasta_file" name="fasta_file"/>
+			<input type="file" id="annotation_file" name="annotation_file"/>
 			</div>
 			</fieldset>
 			<div id='submission'>
@@ -32,7 +32,7 @@
 		</form>
 	</section>
 	<script type="text/javascript">
-		var fileInput = document.querySelector('#fasta_file');
+		var fileInput = document.querySelector('#annotation_file');
 		fileInput.addEventListener('change', function() {
 		var reader = new FileReader();
 		reader.addEventListener('load', function() {

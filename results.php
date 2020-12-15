68980716f974a358de1152a427c05195fb14b1d0
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-15 10:15:21 +0100

    text update

--- results.php
+++ results.php
@@ -167,9 +167,9 @@ include("./includes/header.php");
 		echo "<input type='button' class='bouton_info' value='Search parameters' onclick='close_open_info(this);' />";
 		echo "<div class='contenu_info'>";
 		if($form == 'small' | $form == 'large' | $form == 'clan' | $form == 'visualization_jobID'){
-				echo "<ul><h4>Your job parameters:</h4>";}
+				echo "<ul><strong>Your job parameters:</strong>";}
 		else if($form == 'small_example' | $form == 'large_example' | $form == 'clan_example'){
-				echo "<ul><h4>Example parameters:</h4>";} 
+				echo "<ul><strong>Example parameters:</strong>";} 
 		foreach($parameters as $name => $value){
 			if($name == 'Library' && $value == 'Complete'){
 				echo "<li>Annotation based on the full domain library</li>";}

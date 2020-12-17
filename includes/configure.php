9c57285dee578abc5777ba42c51a4a986031c51e
ClemHub <clemenceblachon@gmail.com>
2020-12-17 10:54:34 +0100

    correction

--- includes/configure.php
+++ includes/configure.php
@@ -1,9 +1,10 @@
 <?php 
-$appname  = 'MetaCLADE_webserver';
+$appname  = 'myclade';
+$appdir = 'MetaCLADE_webserver';
 $appsroot = '/var/www/html'; #It is "/appsdata" on production whatever you want on development. Anyway, has to be accessible to apache
-$approot  = $appsroot.'/'.$appname;  #Directory where the app is hosted. It has to be accessible from apache user
+$approot  = $appsroot.'/'.$appdir;  #Directory where the app is hosted. It has to be accessible from apache user
 //$hostname = 'http://localhost:1234'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
 $hostname = 'http://www.lcqb.upmc.fr'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
-$appurl   = $hostname.'/myclade'; #In production ask the sysadmin for making this address avalaible from the external.
+$appurl   = $hostname.'/'.$appname; #In production ask the sysadmin for making this address avalaible from the external.
 $webdevel = 'clemenceblachon@gmail.com'; #mail address of the webdevelopment
 ?>

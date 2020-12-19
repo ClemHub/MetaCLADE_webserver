b0340f9290b2cc581765bb034f0736c72416061d
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-19 23:41:08 +0100

    MyCLADE instead of MYCLADE

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php include("./includes/header.php"); ?>
 
 	<section id='home'>
-    <h3>MYCLADE: an accurate multi-source domain annotation server designed for a fast exploration of genomic and metagenomic sets of sequences</h3>
+    <h3>MyCLADE: an accurate multi-source domain annotation server designed for a fast exploration of genomic and metagenomic sets of sequences</h3>
 		<p class = 'text'>
 		The understanding of the ever-increasing number of genomic and metagenomic sequences accumulating in our databases demands for approaches that rapidly "explore" the content of sets of (fragmented) protein sequences with respect to specific domain targets,
-		avoiding full domain annotation and full assembly. MYCLADE performs a multi-source domain annotation strategy based on a library of probabilistic domain models associated to each domain. It works in two modes: 
+		avoiding full domain annotation and full assembly. MyCLADE performs a multi-source domain annotation strategy based on a library of probabilistic domain models associated to each domain. It works in two modes: 
 	<ol type="1" class="ordonned_list">
 		<li>It explores large datasets of a few thousands amino-acid sequences and extracts those sequences containing few targeted domains.</li>
 		<li>It annotates small datasets of a few hundreds amino-acid sequences with the full set of Pfam domains.</li>

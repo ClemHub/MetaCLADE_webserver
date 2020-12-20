2110c59dffdc63d02f08dfff092b54822b307bb9
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-20 12:16:41 +0100

    link

--- references.php
+++ references.php
@@ -7,8 +7,8 @@
 	<ul class = spaced_list>
 		<li>R. Vicedomini, C. Blachon and A. Carbone. (2020) MyCLADE: a multi-source domain annotation server for sequence 
 		functional exploration. In preparation.</li>
-		<li>A. Ugarte, R. Vicedomini, J.S. Bernardes and A. Carbone. (2018) A multi-source domain annotation pipeline for quantitative metagenomic and metatranscriptomic functional profiling. Microbiome, 6:149.</li>
-		<li>J.S. Bernardes, F.R.J. Vieira, G. Zaverucha and A. Carbone. (2016) A multi-objective optimization approach accurately resolves protein domain architectures. Bioinformatics, 32(3), 345-353.</li>
+		<li>A. Ugarte, R. Vicedomini, J.S. Bernardes and A. Carbone. (2018) <a href="https://academic.oup.com/bioinformatics/article/32/3/345/1743675" target="_blank" class="table_link">A multi-source domain annotation pipeline for quantitative metagenomic and metatranscriptomic functional profiling.</a> Microbiome, 6:149.</li>
+		<li>J.S. Bernardes, F.R.J. Vieira, G. Zaverucha and A. Carbone. (2016) <a href="hhttps://microbiomejournal.biomedcentral.com/articles/10.1186/s40168-018-0532-2" target="_blank" class="table_link">A multi-objective optimization approach accurately resolves protein domain architectures.</a> Bioinformatics, 32(3), 345-353.</li>
 	</ul>
 
 	</section>

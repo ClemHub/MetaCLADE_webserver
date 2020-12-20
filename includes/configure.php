b9b577d8cb727b28c3681b77392cf6d5030f6c09
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-20 11:36:54 +0100

    cancelling

--- includes/configure.php
+++ includes/configure.php
@@ -5,6 +5,6 @@ $appsroot = '/var/www/html'; #It is "/appsdata" on production whatever you want 
 $approot  = $appsroot.'/'.$appdir;  #Directory where the app is hosted. It has to be accessible from apache user
 //$hostname = 'http://localhost:1234'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
 $hostname = 'http://www.lcqb.upmc.fr'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
-$appurl   = $hostname.'/'.$appname.'/'; #In production ask the sysadmin for making this address avalaible from the external.
+$appurl   = $hostname.'/'.$appname; #In production ask the sysadmin for making this address avalaible from the external.
 $webdevel = 'clemenceblachon@gmail.com'; #mail address of the webdevelopment
 ?>

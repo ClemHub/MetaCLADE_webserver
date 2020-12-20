f4aa4dde6f83b3938e33cbc47ba3821b9c1a0749
Clémence Blachon <clemenceblachon@gmail.com>
2020-12-20 15:02:59 +0100

    jquery

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,7 @@ echo "<head>";
 	echo "<link rel='stylesheet' type='text/css'  href='".$appurl."/css_style/style.css'>";
 	echo "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>";
 	echo "<script src='https://kit.fontawesome.com/a076d05399.js'></script>";
+	echo "<script src='http://code.jquery.com/1-10-2.js'></script>";
 	echo "<title>MyCLADE</title></head>";
 
 

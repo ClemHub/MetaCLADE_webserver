3693f3aca8b971fa88cd94c949660303d6d35a9e
clemblcn <clemenceblachon@gmail.com>
2021-04-07 22:21:41 +0200

    count seq

--- submit.php
+++ submit.php
@@ -64,7 +64,7 @@ include("./includes/header.php");
 				file_put_contents($approot."/jobs/".$job_id."/parameters.txt", "Max domain overlapping (%)\t".$_POST["overlappingMaxDomain_nb"]."\n", FILE_APPEND);
 				$parameters['Max domain overlapping (%)'] = $_POST["overlappingMaxDomain_nb"];}
 			file_put_contents($approot.'/jobs/'.$job_id.'/data.fa', $_POST["sequences"]);
-			file_put_contents($approot."/jobs/".$job_id."/parameters.txt", "Number of sequences\t".preg_match_all("/^>|\n>/", $str)."\n", FILE_APPEND);
+			file_put_contents($approot."/jobs/".$job_id."/parameters.txt", "Number of sequences\t".preg_match_all("/^>|\n>/", $_POST["sequences"])."\n", FILE_APPEND);
 			$msg = submit($job_id, $parameters);
 			echo $msg;
 			$email = $_POST['email'];

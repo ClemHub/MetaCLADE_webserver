660b68c45dbf227bc982bc7215385be7f557176d
clemblcn <clemenceblachon@gmail.com>
2021-04-07 23:30:57 +0200

    minor changes

--- results.php
+++ results.php
@@ -220,7 +220,7 @@ include("./includes/header.php");
 <script>
 
 $(document).ready(function() {
-	var table = $('#count_table').DataTable(dom:'t');
+	var table = $('#count_table').DataTable(dom:'tr');
 });
 </script>
 

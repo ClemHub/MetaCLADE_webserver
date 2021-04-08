67b4a7011690d6027b3889f5e0273c272f5e053b
clemblcn <clemenceblachon@gmail.com>
2021-04-08 23:30:27 +0200

    table

--- results.php
+++ results.php
@@ -159,6 +159,9 @@ include("./includes/header.php");
 				echo "<option value='".$domain."'>".$domain."</option>";}
 			}
 			echo "</select></th>";
+			echo "<th></th>";
+			echo "<th></th>";
+			echo "<th></th>";
 			echo "<th class='table_header'><input id='e-value_max' type='text' placeholder='E-value max'/></th>";
 			echo "<th></th>";
 			echo "</tr>";

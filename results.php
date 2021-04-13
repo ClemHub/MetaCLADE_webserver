9c9291c3dc3a531bd67b71f39a50429e25441d51
clemblcn <clemenceblachon@gmail.com>
2021-04-13 23:55:05 +0200

    test

--- results.php
+++ results.php
@@ -40,7 +40,7 @@ include("./includes/header.php");
 		echo "<div id = 'other_pfam'>";
 		echo "<h5>Other domain:  <span class='tooltip'><i class='far fa-question-circle'></i><span class='tooltiptext'>Select the domains you want to visualize with the first one you selected.</span></span></h5>";
 		foreach($domain_list as $domain_id){
-			echo "<input type='checkbox' id=".$domain_id." name='domain_cb' value=".$domain_id.">";
+			echo "<input type='checkbox' id=".$domain_id." name='domain_cb' value=".trim($domain_id).">";
 			echo "<label for=".$domain_id.">".$domain_id."</label>";}
 		?>
 		</div>

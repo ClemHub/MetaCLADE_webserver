cc7300d20fb5f7c16d018d681ff12f12a4c52c70
clemblcn <clemenceblachon@gmail.com>
2021-04-13 23:47:40 +0200

    test

--- results.php
+++ results.php
@@ -119,7 +119,13 @@ include("./includes/header.php");
 				table.search(this.value).draw();});
 			$('#domain-filter').on('keyup change', function(){
 				table.search(this.value, regex=true).draw()});
-
+				$('input:checkbox').on('change', function () {
+			//build a regex filter string with an or(|) condition
+			var domains = $('input:checkbox[name="domain_cb"]:checked').map(function() {
+				return '^' + this.value + '$';
+			}).get().join('|');
+			//filter in column 1, with an regex, no smart filtering, not case sensitive
+			table.column(1).search(domamins).draw(false);
 		});
 		</script>
 		

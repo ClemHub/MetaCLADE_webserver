27dc09691ce47ec93f17508e3256d94a09238515
clemblcn <clemenceblachon@gmail.com>
2021-04-19 21:42:04 +0200

    mail

--- includes/configure.php
+++ includes/configure.php
@@ -6,5 +6,5 @@ $approot  = $appsroot.'/'.$appdir;  #Directory where the app is hosted. It has t
 //$hostname = 'http://localhost:1234'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
 $hostname = 'http://www.lcqb.upmc.fr'; #hostname is "wwww.lcqb.upmc.fr" in production phase, your host ip during development
 $appurl   = $hostname.'/'.$appname; #In production ask the sysadmin for making this address avalaible from the external.
-$webdevel = 'clemenceblachon@gmail.com'; #mail address of the webdevelopment
+$webdevel = 'clemence.blachon@sorbonne.universite.fr'; #mail address of the webdevelopment
 ?>

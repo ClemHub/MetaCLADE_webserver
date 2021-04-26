d3b65fefd51548e77b2c7a8676b154d8504711ae
clemblcn <clemenceblachon@gmail.com>
2021-04-26 23:46:08 +0200

    new ref

--- references.php
+++ references.php
@@ -9,6 +9,7 @@
 		functional exploration. In preparation.</li>
 		<li>A. Ugarte, R. Vicedomini, J.S. Bernardes and A. Carbone. (2018) <a href="https://microbiomejournal.biomedcentral.com/articles/10.1186/s40168-018-0532-2" target="_blank" class="table_link">A multi-source domain annotation pipeline for quantitative metagenomic and metatranscriptomic functional profiling.</a> Microbiome, 6:149.</li>
 		<li>J.S. Bernardes, F.R.J. Vieira, G. Zaverucha and A. Carbone. (2016) <a href="https://academic.oup.com/bioinformatics/article/32/3/345/1743675" target="_blank" class="table_link">A multi-objective optimization approach accurately resolves protein domain architectures.</a> Bioinformatics, 32(3), 345-353.</li>
+		<li>R. Vicedomini, C. Blachon, F. Oteri and A. Carbone. (2021) MyCLADE: a multi-source domain annotation server for sequence functional exploration. Submitted.</li>
 	</ul>
 
 	</section>

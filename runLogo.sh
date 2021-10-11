f6909ef228bb7498ee3cf11b8faa3dc476c6da56
clemblcn <clemenceblachon@gmail.com>
2021-10-11 11:15:28 +0200

    correction

--- runLogo.sh
+++ runLogo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ../../includes/configure.sh
 echo "LOGO	building" >> parameters.txt
-python3 $APPROOT/get_logodata.py --mclade_cfg ../../metaclade2/config/mclade.complete.cfg  $@
+python3 $APPROOT/get_logodata.py --mclade_cfg ~/Documents/Tools/metaclade2/config/mclade.complete.cfg  $@
 ret_code=$?
 if [ $ret_code = 0 ]; then
   echo "LOGO	true" >> parameters.txt
